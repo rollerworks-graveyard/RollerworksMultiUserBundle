752779f86a56a31cc5b77049b6187fa88c1f3639
Sebastiaan Stok <s.stok@rollerscapes.net>
2014-02-20 15:39:36 +0100

    fix file headers

--- src/Rollerworks/Bundle/MultiUserBundle/Exception/NoActiveUserSystemException.php
+++ src/Rollerworks/Bundle/MultiUserBundle/Exception/NoActiveUserSystemException.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the Symfony package.
+ * This file is part of the RollerworksMultiUserBundle package.
  *
- * (c) Fabien Potencier <fabien@symfony.com>
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
