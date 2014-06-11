baf5a66712d43b41fc13c71e2d923aa5302a2837
Sebastiaan Stok <s.stok@rollerscapes.net>
2014-06-11 16:43:18 +0200

    style: update headers, and add .gush.yml

--- tests/Rollerworks/Bundle/MultiUserBundle/Tests/Functional/Bundle/UserBundle/Form/Type/RegistrationFormType.php
+++ tests/Rollerworks/Bundle/MultiUserBundle/Tests/Functional/Bundle/UserBundle/Form/Type/RegistrationFormType.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the RollerworksMultiUserBundle package.
+ *
+ * (c) 2013 Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Rollerworks\Bundle\MultiUserBundle\Tests\Functional\Bundle\UserBundle\Form\Type;
 
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
--- tests/Rollerworks/Bundle/MultiUserBundle/Tests/Functional/autoload.php
+++ tests/Rollerworks/Bundle/MultiUserBundle/Tests/Functional/autoload.php
@@ -1,3 +1,12 @@
 <?php
 
+/**
+ * This file is part of the RollerworksMultiUserBundle package.
+ *
+ * (c) 2013 Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 require __DIR__ . '/../../../../../bootstrap.php';
